a235b4b0b3ba17e3d3b14b77f27a41d138df37dd
Timfinity4 <168996454+JoelBDirtbike@users.noreply.github.com>
2024-08-25 15:46:56 -0700

    remapped robot

--- Mission1_2024.py
+++ Mission1_2024.py
@@ -1,7 +1,12 @@
 from PortMap24 import *
 
-driveBase.straight(360)
-driveBase.turn(360)
-motorBack.run_angle(1000, 360)
-motorFront.run_angle(1000, 360)
-driveBase.straight(-360)
+driveBase.straight(1000)
+driveBase.turn(30)
+#motorBack.run_angle(1000, 360)
+#motorFront.run_angle(1000, 360)
+driveBase.straight(50)
+driveBase.turn(-30)
+driveBase.straight(50)
+motorFront.run_angle(1000, 90)
+motorFront.run_angle(1000, -90)
+driveBase.straight(-100)
