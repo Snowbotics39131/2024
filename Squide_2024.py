942449d6b4623ea502d7cb4314ec4fdd39c60e74
Timfinity4 <TimothyTacoVender@gmail.com>
2024-11-09 15:49:13 -0800

    commit

---
+++ Squide_2024.py
@@ -0,0 +1,17 @@
+from PortMap24 import * 
+#8,0
+driveBase.settings(straight_speed=90)
+driveBase.straight(350)
+driveBase.straight(300)
+driveBase.straight(-100)
+driveBase.turn(60)
+driveBase.straight(300)
+#driveBase.turn(30)
+#driveBase.straight(250)
+driveBase.turn(-16)
+driveBase.straight(350)
+driveBase.turn(45)
+driveBase.straight(180)
+motorFront.run_angle(100,360)
+
+
