c1fd60d171cf094fd14c0edd91a87538da17d90f
JeremiahMohan <jeremiah.mohan@icloud.com>
2024-11-09 17:57:33 -0700

    changes

--- robot2024.py
+++ robot2024.py
@@ -1,25 +1,17 @@
 from PortMap24 import *
 
-mission = hub_menu(1,2,3,4)
+mission = hub_menu(1,2,3,4,5)
 print(mission)
 
 if mission == 1:
-    import Mission1_2024
+    import mission1_2024
 if mission == 2:
     import mission3_2024
 if mission == 3:
     import mission2_2024
+#if mission == 4:
+   # import 
 if mission == 4:
-    import 
-if mission == 5:
     import rightmap
-if mission == 6:
+if mission == 5:
     import whaleandkrill
-if mission == 7:
-    import
-if mission == 8:
-    import
-if mission == 9:
-    import 
-if mission == 10:
-    import
