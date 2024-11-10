f63c64a01ca2d06a8e1959c21c7334ee3bfedaeb
JeremiahMohan <jeremiah.mohan@icloud.com>
2024-11-09 17:05:56 -0700

    rightside

--- robot2024.py
+++ robot2024.py
@@ -4,8 +4,22 @@ mission = hub_menu(1,2,3,4)
 print(mission)
 
 if mission == 1:
-    import mission1_2024
+    import Mission1_2024
 if mission == 2:
     import mission3_2024
 if mission == 3:
     import mission2_2024
+if mission == 4:
+    import 
+if mission == 5:
+    import rightmap
+if mission == 6:
+    import whaleandkrill
+if mission == 7:
+    import
+if mission == 8:
+    import
+if mission == 9:
+    import 
+if mission == 10:
+    import
