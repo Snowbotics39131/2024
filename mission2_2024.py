7ec325704ac74315c59517239cdb64a50da02538
thebuilder6 <43260133+thebuilder6@users.noreply.github.com>
2024-11-09 17:16:05 -0700

    Update mission2_2024.py

--- mission2_2024.py
+++ mission2_2024.py
@@ -1,7 +1,12 @@
 from PortMap24 import * 
-
-driveBase.straight(100)
-driveBase.turn(90)
-driveBase.straight(2000)
-driveBase.turn(-90)
-driveBase.straight(100)
+#1,4
+driveBase.settings(straight_speed=800)
+driveBase.settings(turn_rate=800)
+driveBase.turn(-17)
+driveBase.straight(750)
+driveBase.straight(-200)
+driveBase.turn(17)
+driveBase.straight(1000)
+driveBase.turn(45)
+driveBase.straight(-300)
+driveBase.straight(600)
