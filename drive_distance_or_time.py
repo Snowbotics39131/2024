cc7f8e0c23de75637a1aaf5185c5e273ab6ce6be
Uxugin <git@uxugin.com>
2024-11-16 10:51:16 -0700

    turn angle or time

--- drive_distance_or_time.py
+++ drive_distance_or_time.py
@@ -8,9 +8,20 @@ def drive_distance_or_time(distance: int, max_time=10000, then=Stop.HOLD):
         hub.speaker.beep()
         print(f"Time limit of {max_time}ms exhausted while driving {distance}mm.")
         driveBase.stop()
+def turn_angle_or_time(angle: int, max_time=5000, then=Stop.HOLD):
+    driveBase.turn(angle, then=then, wait=False)
+    stop_watch = StopWatch()
+    while not(driveBase.done()) and stop_watch.time() <= max_time:
+        pass
+    if not driveBase.done():
+        hub.speaker.beep()
+        print(f"Time limit of {max_time}ms exhausted while turning {angle} degrees.")
+        driveBase.stop()
 
 if __name__ == '__main__':
-    drive_distance_or_time(200, 3000)
+    for _ in range(4):
+        drive_distance_or_time(200, 3000)
+        turn_angle_or_time(90, 3000)
 
 
 
