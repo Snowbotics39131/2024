33a57f32419a837712eec179c86042a71d1b780a
JeremiahMohan <jeremiah.mohan@icloud.com>
2024-11-26 18:34:14 -0700

    added

---
+++ robot2024.py
@@ -0,0 +1,25 @@
+from PortMap24 import *
+
+mission = hub_menu(1,2,3,4,5,6,7,8,9,10)
+print(mission)
+
+if mission == 1:
+    import mission1_2024_2
+if mission == 8:
+    import mission3_2024
+if mission == 7:
+    import mission2_2024
+if mission == 5:
+    import Test2
+if mission == 9:
+    import rightmap
+if mission == 4:
+    import joels_code
+if mission == 6:
+    import squid3
+if mission == 10:
+    import whaleandkrill
+if mission == 2:
+    import tree_2024
+if mission == 3:
+    import Tree_part2_2024
