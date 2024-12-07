d44ffa32200d52e7ee6c4d7ff12476b5660770b8
JeremiahMohan <jeremiah.mohan@icloud.com>
2024-12-07 09:49:41 -0700

    new

--- Tree_part2_2024.py
+++ Tree_part2_2024.py
@@ -1,7 +1,13 @@
 from PortMap24 import * 
-
-driveBase.settings(straight_speed=350)
-motorFront.run_until_stalled(100,Stop.COAST,100)
-driveBase.straight(400)
+#2nd thick black line from front on edge. 
+#1 square from left. facing toward corral tree
+async def aaaaa():
+    motorFront.run_until_stalled(100,Stop.COAST,100)
+    await driveBase.straight(400)
+driveBase.settings(straight_speed=150)
+run_task(aaaaa())
+#motorFront.run_angle(1000,10, wait=False)
+#wait(100)
 motorFront.run_until_stalled(-1000,Stop.COAST,30)
+driveBase.settings(straight_speed=600)
 driveBase.straight(-400)
